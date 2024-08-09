ac172cfc4579d6c2d7e4425639918068634a7b72
Matheus <matheuscviana22@gmail.com>
2024-08-09 16:19:27 -0300

    noticia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { initFlowbite } from 'flowbite/lib/esm/components';
 import { MeioSiteComponent } from "./meio-site/meio-site.component";
+import { NoticiaComponent } from "./noticia/noticia.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, MeioSiteComponent],
+  imports: [RouterOutlet, NavbarComponent, MeioSiteComponent, NoticiaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
